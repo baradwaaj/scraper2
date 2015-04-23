0da52b9a2cc90757133aac9ffc9fd3fbf3918659
baradwaaj <brady6.38b@gmail.com>
2015-04-23 09:58:54 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,6 @@ import sys
 item_count = 600
 for i in range(0, item_count, 40):
     link = "http://www.flipkart.com/mobiles/pr?p%5B%5D=sort%3Dprice_asc&sid=tyy%2C4io&layout=grid&ajax=true&start=%d" % (i+1)
-
     
     print link
 response = urllib2.urlopen(link)
