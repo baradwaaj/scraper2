dc38dcd03d5646d2afbc9d7164cded38f6ff31bb
baradwaaj <brady6.38b@gmail.com>
2015-04-23 09:55:59 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,33 +1,10 @@
-import urllib2
-
 import bs4
+import re
+import urllib2
+import sys
 
-
-def findItem(itemName):
-    itemName.replace(" ", "+")
-    link = 'http://www.flipkart.com/search/a/all?query= {0}&vertical=all&dd=0&autosuggest[as]=off&autosuggest[as-submittype]=entered&autosuggest[as-grouprank]=0&autosuggest[as-overallrank]=0&autosuggest[orig-query]=&autosuggest[as-shown]=off&Search=%C2%A0&otracker=start&_r=YSWdYULYzr4VBYklfpZRbw--&_l=pMHn9vNCOBi05LKC_PwHFQ--&ref=a2c6fadc-2e24-4412-be6a-ce02c9707310&selmitem=All+Categories'.format(
-        itemName)
-    r = urllib2.Request(link, headers={"User-Agent": "Python-urlli~"})
-    try:
-        response = urllib2.urlopen(r)
-    except:
-        print "Internet connection error"
-        print urllib2.urlopen(r)
-        return
-    thePage = response.read()
-    soup = bs4.BeautifulSoup(thePage)
-
-    firstBlockSoup = soup.find('div', attrs={'class': 'product-unit'})
-    if not firstBlockSoup:
-        firstBlockSoup = soup.find('div', attrs={'class': 'size1of4 fk-medium-atom unit'})
-        if not firstBlockSoup:
-            print "Item Not Found"
-            return
-
-    print "Item found"
-    return
-
-
-findItem("galaxy s advance")
-findItem("Giordano Analog Watch")
-findItem("nosuchitemfound")
+link = 'http://www.flipkart.com/mobiles/pr?p[]=sort%3Dprice_asc&sid=tyy%2C4io&layout=grid'
+response = urllib2.urlopen(link)
+thePage = response.read()
+soup = bs4.BeautifulSoup(thePage)
+allMobiles = soup.find('div', attrs={'id': 'products'})
