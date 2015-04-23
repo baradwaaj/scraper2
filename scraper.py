6b00a9d657d5df94e804d228f06012d1e928816f
baradwaaj <brady6.38b@gmail.com>
2015-04-23 09:51:57 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,6 +12,7 @@ def findItem(itemName):
         response = urllib2.urlopen(r)
     except:
         print "Internet connection error"
+        print response
         return
     thePage = response.read()
     soup = bs4.BeautifulSoup(thePage)
